e40cad55a32ea20b5fe1c3645dad142acffbf4f1
malwaredb86 <mdurham86@gmail.com>
2023-08-06 21:00:46 -0400

    Completed exercise

--- part2/exercises-2/src/components/Button.js
+++ part2/exercises-2/src/components/Button.js
@@ -2,10 +2,14 @@ import './styles.css';
 
 function Button() {
 
-   return ( 
-         <button>
-            Learn More
-         </button>
+   function onLearnMore() {
+      alert("Splish Splash!");
+   };
+
+   return (
+      <button onClick={onLearnMore}>
+         Learn More
+      </button>
    );
 }
 
--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -1,14 +1,36 @@
 import './styles.css';
 import Button from './Button';
+import ocean from './oceans.json'
 
 
-function Profile()
-{
-   return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
+const listItem = ocean.map(ocean =>
+   <div key={ocean.id} className=
+      {`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
+      <img src={ocean.image} alt={`An image of ${ocean.name}`} className="img"></img>
+      <h1>{ocean.name}</h1>
+      <h2>Fun facts!</h2>
+      <ol>
+         <l1>{ocean.fact1}.</l1>
+         <br></br>
+         <l1>{ocean.fact2}.</l1>
+         <br></br>
+         <l1>{ocean.fact3}.</l1>
+      </ol>
+      <br></br>
+      <Button />
+   </div>
+)
+
+   ;
+
+function Profile() {
+   return (
+
+      <ul>
+         {listItem}
+      </ul>
+
+
    );
 }
 
